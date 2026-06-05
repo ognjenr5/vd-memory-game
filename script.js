18d933a884cb5e69aec025cf9a4e1974529db219
(k)buntu-ognjen <52324253+ognjenr5@users.noreply.github.com>
2026-06-05 03:19:28 +0200

    Add main page functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let cards = []
+let gameStarted = false
 
 function swap (i, j) {
   let tmp = cards[i]
@@ -33,6 +34,31 @@ function displayCards () {
   }
 }
 
-$(document).ready(function () {
+function mainPageHandler () {
+  $('button.mode').click(function () {
+    if ($(this).hasClass('selected-mode')) return
+    $('button.mode').toggleClass('selected-mode')
+  })
+
+  $('#start').click(function () {
+    localStorage.setItem('level', parseInt($('#lvl').find(':selected').val()))
+    localStorage.setItem('mode', (mode = $('.selected-mode').attr('id')))
+    window.location.href = './game.html'
+  })
+}
+
+function gamePageHandler () {
   displayCards()
+
+  let level = parseInt(localStorage.getItem('level'))
+  let mode = localStorage.getItem('mode')
+
+  gameStarted = true
+
+  console.log(level + '\n' + mode)
+}
+
+$(document).ready(function () {
+  if ($('body').hasClass('main-page')) mainPageHandler()
+  else if ($('body').hasClass('game-page')) gamePageHandler()
 })
