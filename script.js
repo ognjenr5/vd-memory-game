bbd5d4006aa1b3e32d49209c8b65cc035e29e678
(k)buntu-ognjen <52324253+ognjenr5@users.noreply.github.com>
2026-06-06 21:55:35 +0200

    Add timed mode and score logic

--- script.js
+++ script.js
@@ -1,5 +1,138 @@
 let cards = []
 let gameFinished
+let scoreboardTimeHard = [
+  { user: 'AAA', score: 0 },
+  { user: 'BBB', score: 0 },
+  { user: 'CCC', score: 0 },
+  { user: 'DDD', score: 0 },
+  { user: 'EEE', score: 0 },
+  { user: 'FFF', score: 0 },
+  { user: 'GGG', score: 0 },
+  { user: 'HHH', score: 0 },
+  { user: 'III', score: 0 },
+  { user: 'JJJ', score: 0 }
+]
+
+let scoreboardTimeMedium = [
+  { user: 'AAA', score: 0 },
+  { user: 'BBB', score: 0 },
+  { user: 'CCC', score: 0 },
+  { user: 'DDD', score: 0 },
+  { user: 'EEE', score: 0 },
+  { user: 'FFF', score: 0 },
+  { user: 'GGG', score: 0 },
+  { user: 'HHH', score: 0 },
+  { user: 'III', score: 0 },
+  { user: 'JJJ', score: 0 }
+]
+
+let scoreboardTimeEasy = [
+  { user: 'AAA', score: 0 },
+  { user: 'BBB', score: 0 },
+  { user: 'CCC', score: 0 },
+  { user: 'DDD', score: 0 },
+  { user: 'EEE', score: 0 },
+  { user: 'FFF', score: 0 },
+  { user: 'GGG', score: 0 },
+  { user: 'HHH', score: 0 },
+  { user: 'III', score: 0 },
+  { user: 'JJJ', score: 0 }
+]
+
+let scoreboardStepsHard = [
+  { user: 'AAA', score: 999 },
+  { user: 'BBB', score: 999 },
+  { user: 'CCC', score: 999 },
+  { user: 'DDD', score: 999 },
+  { user: 'EEE', score: 999 },
+  { user: 'FFF', score: 999 },
+  { user: 'GGG', score: 999 },
+  { user: 'HHH', score: 999 },
+  { user: 'III', score: 999 },
+  { user: 'JJJ', score: 999 }
+]
+
+let scoreboardStepsMedium = [
+  { user: 'AAA', score: 999 },
+  { user: 'BBB', score: 999 },
+  { user: 'CCC', score: 999 },
+  { user: 'DDD', score: 999 },
+  { user: 'EEE', score: 999 },
+  { user: 'FFF', score: 999 },
+  { user: 'GGG', score: 999 },
+  { user: 'HHH', score: 999 },
+  { user: 'III', score: 999 },
+  { user: 'JJJ', score: 999 }
+]
+
+let scoreboardStepsEasy = [
+  { user: 'AAA', score: 999 },
+  { user: 'BBB', score: 999 },
+  { user: 'CCC', score: 999 },
+  { user: 'DDD', score: 999 },
+  { user: 'EEE', score: 999 },
+  { user: 'FFF', score: 999 },
+  { user: 'GGG', score: 999 },
+  { user: 'HHH', score: 999 },
+  { user: 'III', score: 999 },
+  { user: 'JJJ', score: 999 }
+]
+
+function loadScoreBoard () {
+  let _scoreboardTimeHard = localStorage.getItem('scoreboardTimeHard')
+  if (_scoreboardTimeHard != null) {
+    scoreboardTimeHard = JSON.parse(_scoreboardTimeHard)
+  } else
+    localStorage.setItem(
+      'scoreboardTimeHard',
+      JSON.stringify(scoreboardTimeHard)
+    )
+
+  let _scoreboardTimeMedium = localStorage.getItem('scoreboardTimeMedium')
+  if (_scoreboardTimeMedium != null) {
+    scoreboardTimeMedium = JSON.parse(_scoreboardTimeMedium)
+  } else
+    localStorage.setItem(
+      'scoreboardTimeMedium',
+      JSON.stringify(scoreboardTimeMedium)
+    )
+
+  let _scoreboardTimeEasy = localStorage.getItem('scoreboardTimeEasy')
+  if (_scoreboardTimeEasy != null) {
+    scoreboardTimeEasy = JSON.parse(_scoreboardTimeEasy)
+  } else
+    localStorage.setItem(
+      'scoreboardTimeEasy',
+      JSON.stringify(scoreboardTimeEasy)
+    )
+
+  let _scoreboardStepsHard = localStorage.getItem('scoreboardStepsHard')
+  if (_scoreboardStepsHard != null) {
+    scoreboardStepsHard = JSON.parse(_scoreboardStepsHard)
+  } else
+    localStorage.setItem(
+      'scoreboardStepsHard',
+      JSON.stringify(scoreboardStepsHard)
+    )
+
+  let _scoreboardStepsMedium = localStorage.getItem('scoreboardStepsMedium')
+  if (_scoreboardStepsMedium != null) {
+    scoreboardStepsMedium = JSON.parse(_scoreboardStepsMedium)
+  } else
+    localStorage.setItem(
+      'scoreboardStepsMedium',
+      JSON.stringify(scoreboardStepsMedium)
+    )
+
+  let _scoreboardStepsEasy = localStorage.getItem('scoreboardStepsEasy')
+  if (_scoreboardStepsEasy != null) {
+    scoreboardStepsEasy = JSON.parse(_scoreboardStepsEasy)
+  } else
+    localStorage.setItem(
+      'scoreboardStepsEasy',
+      JSON.stringify(scoreboardStepsEasy)
+    )
+}
 
 function swap (i, j) {
   let tmp = cards[i]
@@ -89,12 +222,14 @@ function createCardGrid (level) {
   $('.picture-grid').append(table)
 }
 
-function classicGame () {
+function classicGame (level) {
   let steps = 0
   let picBlocked = false
   let firstOpened = false
   let secondOpened = false
   let first, second
+  let cardCount = level == 1 ? 3 : level == 2 ? 8 : 15
+  let currCards = 0
 
   $('#span-mode-info').text('Broj koraka: ')
   $('#span-mode').text(steps)
@@ -115,6 +250,10 @@ function classicGame () {
       second = $(this)
       if (first.attr('card') == second.attr('card')) {
         firstOpened = false
+        currCards++
+        if (cardCount == currCards) {
+          gameEnd(true, steps, 'classic')
+        }
       } else {
         picBlocked = true
         setTimeout(() => {
@@ -128,6 +267,101 @@ function classicGame () {
   })
 }
 
+function timedGame (level) {
+  let timeSeconds = level == 1 ? 15 : level == 2 ? 90 : 180
+  let picBlocked = false
+  let firstOpened = false
+  let secondOpened = false
+  let first, second
+  let cardCount = level == 1 ? 3 : level == 2 ? 8 : 15
+  let currCards = 0
+
+  $('#span-mode-info').text('Vreme: ')
+  $('#span-mode').text(
+    parseInt(Math.floor(timeSeconds / 60)) +
+      ':' +
+      ('0' + (timeSeconds % 60)).slice(-2)
+  )
+
+  let interval = setInterval(function () {
+    timeSeconds--
+    $('#span-mode').text(
+      parseInt(Math.floor(timeSeconds / 60)) +
+        ':' +
+        ('0' + (timeSeconds % 60)).slice(-2)
+    )
+  }, 1000)
+
+  let timeout = setTimeout(function () {
+    clearInterval(interval)
+    gameEnd(false, null, null)
+    console.log('Game finished: ' + gameFinished)
+  }, timeSeconds * 1000)
+
+  $('.picture-card').click(function () {
+    if (picBlocked || gameFinished) return
+    if (!$(this).hasClass('hidden')) return
+
+    if (!firstOpened) {
+      firstOpened = true
+      $(this).toggleClass('hidden')
+      first = $(this)
+    } else {
+      $(this).toggleClass('hidden')
+      second = $(this)
+      if (first.attr('card') == second.attr('card')) {
+        firstOpened = false
+        currCards++
+        if (cardCount == currCards) {
+          clearInterval(interval)
+          clearTimeout(timeout)
+          gameEnd(true, timeSeconds, 'timed')
+        }
+      } else {
+        picBlocked = true
+        setTimeout(() => {
+          picBlocked = false
+          first.toggleClass('hidden')
+          second.toggleClass('hidden')
+          firstOpened = false
+        }, 750)
+      }
+    }
+  })
+}
+
+function gameEnd (hasWon, score, scoreType) {
+  gameFinished = true
+  $('.end-game').toggleClass('hidden-end')
+  $('#playAgainDiv').css('display', '')
+  if (hasWon) {
+    $('.status-game').text('Pobedili ste!')
+    $('.add-score').text('Upisite ime u tabelu?')
+    $('#addScoreDiv').css('display', '')
+
+    $('#addToScore').click(function () {
+      loadScoreBoard()
+      let newScore = {
+        user: $('#name').val(),
+        score: score
+      }
+      addScore(newScore, scoreType == 'timed')
+      window.location.href = './score.html'
+    })
+  } else {
+    $('.status-game').text('Izgubili ste!')
+    $('#addScoreDiv').css('display', 'none')
+  }
+
+  $('#playAgain').click(function () {
+    location.reload()
+  })
+
+  $('#returnToStart').click(function () {
+    window.location.href = './index.html'
+  })
+}
+
 function gamePageHandler () {
   let level = parseInt(localStorage.getItem('level'))
   let mode = localStorage.getItem('mode')
@@ -138,12 +372,132 @@ function gamePageHandler () {
 
   $('#span-level').text(level == 1 ? 'Lako' : level == 2 ? 'Srednje' : 'Tesko')
 
-  if (mode == 'classic') classicGame()
-
   gameFinished = false
+  if (mode == 'classic') classicGame(level)
+  else timedGame(level)
+}
+
+function getBoardForScore (isTime) {
+  if (isTime) {
+    switch (localStorage.getItem('level')) {
+      case '1':
+        return scoreboardTimeEasy
+
+      case '2':
+        return scoreboardTimeMedium
+
+      case '3':
+        return scoreboardTimeHard
+    }
+  } else {
+    switch (localStorage.getItem('level')) {
+      case '1':
+        return scoreboardStepsEasy
+
+      case '2':
+        return scoreboardStepsMedium
+
+      case '3':
+        return scoreboardStepsHard
+    }
+  }
+}
+
+function saveBoard (isTime) {
+  if (isTime) {
+    switch (localStorage.getItem('level')) {
+      case '1':
+        return localStorage.setItem(
+          'scoreboardTimeEasy',
+          JSON.stringify(scoreboardTimeEasy)
+        )
+
+      case '2':
+        return localStorage.setItem(
+          'scoreboardTimeMedium',
+          JSON.stringify(scoreboardTimeMedium)
+        )
+
+      case '3':
+        return localStorage.setItem(
+          'scoreboardTimeHard',
+          JSON.stringify(scoreboardTimeHard)
+        )
+    }
+  } else {
+    switch (localStorage.getItem('level')) {
+      case '1':
+        return localStorage.setItem(
+          'scoreboardStepsEasy',
+          JSON.stringify(scoreboardStepsEasy)
+        )
+
+      case '2':
+        return localStorage.setItem(
+          'scoreboardStepsMedium',
+          JSON.stringify(scoreboardStepsMedium)
+        )
+
+      case '3':
+        return localStorage.setItem(
+          'scoreboardStepsHard',
+          JSON.stringify(scoreboardStepsHard)
+        )
+    }
+  }
+}
+
+function addScore (score, isTime) {
+  let board = getBoardForScore(isTime)
+  let i = 0
+  while (
+    i < board.length &&
+    ((isTime && board[i].score >= score.score) ||
+      (!isTime && board[i].score <= score.score))
+  )
+    i++
+  if (i == board.length) return
+  for (let j = board.length - 1; j > i; j--) board[j] = board[j - 1]
+  board[i] = score
+  saveBoard(isTime)
+}
+
+function displayScores (scores, isTime) {
+  let count = 0
+  let table = $('.scoreboard table')
+  scores.forEach(row => {
+    let tr = $('<tr>')
+    let pos = $('<td>')
+      .addClass('position')
+      .text(++count + '.')
+    let name = $('<td>').addClass('name').text(row.user)
+    let sc = $('<td>')
+      .addClass('score-res')
+      .text(
+        isTime
+          ? parseInt(Math.floor(row.score / 60)) +
+              ':' +
+              ('0' + (row.score % 60)).slice(-2)
+          : row.score
+      )
+    tr.append(pos).append(name).append(sc)
+    table.append(tr)
+  })
+}
+
+function scorePageHandler () {
+  loadScoreBoard()
+  if (localStorage.getItem('mode') == 'timed')
+    displayScores(getBoardForScore(true), true)
+  else displayScores(getBoardForScore(false), false)
+
+  $('#returnToStart').click(function () {
+    window.location.href = './index.html'
+  })
 }
 
 $(document).ready(function () {
   if ($('body').hasClass('main-page')) mainPageHandler()
   else if ($('body').hasClass('game-page')) gamePageHandler()
+  else scorePageHandler()
 })
